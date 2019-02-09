69c42bd231199298d4813bd4faca8cd40fc3d639
rjbultitude <richard.bultitude@gmail.com>
2019-02-09 23:19:17 +0000

    attempt to pass params in POST body

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
+const searchTerm = 'genre=Electronic';
+const params = {sort: 'year', sort_order: 'asc', page: 1, per_page: 100};
+const getHeader = {
+  mode: 'no-cors',
+  method: 'get',
+  credentials: 'include'
+};
+const postBody = JSON.stringify({
+  params: params,
+  searchTerm: searchTerm
+});
+const postHeader = {
+  mode: 'no-cors',
+  method: 'POST',
+  credentials: 'include',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: postBody
+};
+
+
 function isCassette(release) {
   console.log('release.format', release.format);
   for (let index = 0; index < release.format.length; index++) {
@@ -11,17 +34,43 @@ function filterResults(res, filterCb) {
   return res.filter(isCassette);
 }
 
-const params = {sort: 'year', sort_order: 'asc', page: 1, per_page: 100};
-const searchTerm = 'genre=Electronic';
+function handleDataSearch(res) {
+  res.then((db) => {
+    console.log('db', db);
+    db.search(searchTerm, params, (err, res) => {
+      if (err !== null) {
+        console.warn(err);
+      } else {
+        const cassList = filterResults(res.results, isCassette);
+        const cassListTitles = cassList.map((release) => {
+          return release.title;
+        });
+        console.log('cassListTitles', cassListTitles);
+      }
+    });
+  });
+}
 
-const res = db.search(searchTerm, params, (err, res)=> {
-  if (err !== null) {
-    console.warn('there was an error: ', err);
-  } else {
+function handleData(res) {
+  //res.then((db) => {
     const cassList = filterResults(res.results, isCassette);
     const cassListTitles = cassList.map((release) => {
       return release.title;
     });
     console.log('cassListTitles', cassListTitles);
-  }
-});
+  //});
+}
+
+fetch(`http://localhost:8080/api/search`, postHeader)
+  .then((response) => {
+    if (response.ok) {
+      console.log('response', response);
+      //handleDataSearch(response.json());
+    } else {
+      throw err;
+    }
+  })
+  .catch((err) => {
+    console.warn(err);
+  });
+

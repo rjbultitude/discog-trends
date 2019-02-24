8d443d10add941ac6240d2de6e5bf99acbd3ec95
rjbultitude <richard.bultitude@gmail.com>
2019-02-24 18:43:20 +0000

    install node cors and configure

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const Discogs = require('disconnect').Client;
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const creds = require('./server/creds.js');
 const app = express();
 const port = 8080;
 const db = new Discogs(creds).database();
 
+// Setup CORS
+const whitelist = [`http://localhost:${port}`, 'http://discogstrends.com'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
 //app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post('/api/search', function (req, res) {
+app.post('/api/search', cors(corsOptions), function (req, res) {
   console.log('req.headers', req.headers);
   console.log('req.body', req.body);
   console.log('typeof req.body', typeof req.body);

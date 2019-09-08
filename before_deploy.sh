f9c2aeffce4ea87db83fa49e3f53554898097245
rjbultitude <richard.bultitude@gmail.com>
2019-09-08 13:37:16 +0100

    travis jobs updates

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,8 +1,3 @@
-# please clone this url https://fe-ols@azp-ms-web-dev-ols.scm.azurewebsites.net:443/AZP-MS-WEB-DEV-OLS.git
-# to a local directory within frontend. You need to set up a deployment credential for the AZP-MS-WEB-DEV-OLS
-#/bin/bash
-npm run build:prod
-
 # get the current branch name and create a directory destination using it
 dest_dir="$(git rev-parse --abbrev-ref HEAD)"
 

eb282c78de10101422976d2d533ded35bd9b921f
rjbultitude <richard.bultitude@gmail.com>
2019-09-10 23:06:47 +0100

    install and configure stylelint and prettier-stylelint. add necessary scripts and hooks

--- app/app.js
+++ app/app.js
@@ -6,22 +6,27 @@ import Filter from './components/filter/filter';
 const GlobalStyle = createGlobalStyle`
   html {
     @import url('https://fonts.googleapis.com/css?family=Saira+Extra+Condensed');
+
     font-size: 1em;
   }
+
   body {
     font-family: 'Franklin Gothic', sans-serif;
   }
+
   h1 {
     font-family: 'Saira Extra Condensed', sans-serif;
     text-transform: uppercase;
     font-weight: normal;
     font-size: 2.2rem;
   }
+
   select,
   option,
   input {
     font-size: 1rem;
-  }`;
+  }
+`;
 
 const App = () => {
   return (

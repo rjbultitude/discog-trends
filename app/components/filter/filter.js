e186432ce16ec3811b4e58394c68fd5649b93a60
rjbultitude <richard.bultitude@gmail.com>
2020-01-04 19:24:28 +0000

    add country field

--- app/components/filter/filter.js
+++ app/components/filter/filter.js
@@ -22,26 +22,42 @@ export default class Filter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      originalData: [],
       releaseData: [],
       pagination: null,
       genre: '',
       format: '',
+      country: '',
     };
     this.changeGenre = this.changeGenre.bind(this);
     this.changeFormat = this.changeFormat.bind(this);
+    this.changeCountry = this.changeCountry.bind(this);
     this.prevResults = this.prevResults.bind(this);
     this.nextResults = this.nextResults.bind(this);
     this.page = 1;
   }
 
+  componentDidMount() {
+    this.getNewData();
+  }
+
   getNewData() {
-    const { genre } = this.state;
-    const { format } = this.state;
-    const query = `genre=${genre},format=${format}`;
+    const { genre, format, country } = this.state;
+    let query = '';
+    if (genre && genre !== '--') {
+      query += `genre=${genre},`;
+    }
+    if (format && format !== '--') {
+      query += `format=${format},`;
+    }
+    if (country && country !== '--') {
+      query += `country=${country},`;
+    }
     getDiscogsData(
       data => {
         const processedData = processData(data.results);
         this.setState({
+          originalData: data.results,
           releaseData: processedData,
           pagination: data.pagination,
         });
@@ -60,6 +76,12 @@ export default class Filter extends React.Component {
 
   changeFormat(e) {
     this.setState({ format: e.target.value }, () => {
+      this.getNewData();
+    });
+  }
+
+  changeCountry(e) {
+    this.setState({ country: e.target.value }, () => {
       this.getNewData();
     });
   }
@@ -99,6 +121,14 @@ export default class Filter extends React.Component {
             selectOptions={appConstants.FORMATS}
             changeCB={this.changeFormat}
             id={appConstants.FORMATS_STR}
+          />
+        </FilterField>
+        <FilterField>
+          <Label text="Country" forVal={appConstants.COUNTRIES_STR} />
+          <Select
+            selectOptions={appConstants.COUNTRIES}
+            changeCB={this.changeCountry}
+            id={appConstants.COUNTRIES_STR}
           />
         </FilterField>
         {releaseData && releaseData.length > 0 ? (
--- app/utils/constants.js
+++ app/utils/constants.js
@@ -4,22 +4,24 @@ export const VINYL_STRING = 'Vinyl';
 export const CD_STRING = 'CD';
 export const GENRES_STR = 'genres';
 export const FORMATS_STR = 'formats';
+export const COUNTRIES_STR = 'countries';
 export const FORMATS = ['--', CASS_STRING, VINYL_STRING, CD_STRING];
+export const COUNTRIES = ['--', 'US', 'UK', 'Germany', 'France', 'Japan'];
 export const GENRES = [
   '--',
   'Rock',
   'Electronic',
   'Pop',
-  `Folk${encodeURIComponent(',')} World ${encodeURIComponent('&')} Country`,
+  'Folk, World & Country',
   'Jazz',
-  `Funk ${encodeURIComponent('/')} Soul`,
+  `Funk / Soul`,
   'Classical',
   'Hip Hop',
   'Latin',
   'Reggae',
-  `Stage ${encodeURIComponent('&')} Screen`,
+  `Stage & Screen`,
   'Blues',
   'Non-Music',
   "Children's",
-  `Brass ${encodeURIComponent('&')} Military`,
+  `Brass & Military`,
 ];

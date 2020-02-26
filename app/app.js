bb5c8544960c129121f11008c2e8df36d827be59
rjbultitude <richard.bultitude@gmail.com>
2020-02-26 21:38:17 +0000

    remove Class component filter now that hooks version is complete

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createGlobalStyle } from 'styled-components';
-import Filter from './components/filter/filter-hooks';
+import Filter from './components/filter/filter';
 
 const GlobalStyle = createGlobalStyle`
   html {

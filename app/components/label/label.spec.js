44bed241a0023fabccd4e83500fbd00230642870
rjbultitude <richard.bultitude@gmail.com>
2020-08-30 16:41:34 +0100

    add label spec and snapshot

---
+++ app/components/label/label.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Label from './Label';
+
+configure({ adapter: new Adapter() });
+
+describe('Label', () => {
+  it('should render when valid forVal and text props are received', () => {
+    const component = shallow(<Label forVal="Test" text="Test" />);
+    expect(component).toMatchSnapshot();
+  });
+});
